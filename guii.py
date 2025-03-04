4da9c52fdee9c385ed05183a8c2920ad560a0eef
afeez <afeezademuyiwa5@gmail.com>
2025-03-04 13:08:57 +0100

    implement add button

--- guii.py
+++ guii.py
@@ -1,11 +1,26 @@
 import Functions
 import FreeSimpleGUI as sg
 
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="enter to-do")
-add_button =sg.Button("Add")
-edit_button = sg.Button("Edit")
 
-window= sg.Window('My TO-DO App', layout=[[label],[input_box, add_button,]])
-window.read()
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="enter to-do",key="todo")
+add_button =sg.Button("Add")
+
+window= sg.Window('My TO-DO App',
+                  layout=[[label],[input_box, add_button,]],
+                  font=('Helvetica', 20))
+while True:
+    event, values =window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos= Functions.get_todos()
+            new_todo= values['todo'] + '\n'
+            todos.append(new_todo)
+            Functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
